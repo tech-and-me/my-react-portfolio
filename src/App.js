5ca01dfc144a8ac693e4d8808e7e868971f8880c
Pary <81phary@gmail.com>
2021-10-26 17:51:38 +0700

    finishing codes for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import  'react-bootstrap-v5'
+import Navbar from "./components/Navbar"
+
 
 
 function App() {
   return (
     <div>
-        <h2>My Portfolio</h2>
-        <button class="btn btn-danger mx-5">Test Button</button>
+        <Navbar />
     </div>
     
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react"
+import logo from "../logo.png"
+
+
+const Navbar =()=> {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+  <divc className="container">
+    <a className="navbar-brand" href="#"><img src={logo} alt="logo" className="logo"/></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon bg-light"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">About me</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Experience</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Portfolio</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Contact</a>
+            </li>
+        </ul>
+          
+      
+    </div>
+  </divc>
+</nav>
+        </div>
+    )
+}
+
+export default Navbar
